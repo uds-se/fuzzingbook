2d633cca55a58ecd2697ba721a6263baaa5343e4
Andreas Zeller <zeller@cs.uni-saarland.de>
2018-06-06 10:53:56 +0200

    New: Use @extend_class to define a class method-by-method

--- gstbook/__init__.py
+++ gstbook/__init__.py
@@ -1,5 +1,5 @@
 # Define the contents of this file as a package
-__all__ = ["PrettyTable"]
+__all__ = ["PrettyTable", "extend_class"]
 
 # Setup loader such that workbooks can be imported directly
 from . import setup
