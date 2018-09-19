9d16551d9fda2daa5ab56359a62a99474ae780ac
Andreas Zeller <zeller@cs.uni-saarland.de>
2018-09-19 11:20:30 +0200

    New: favicon with transparency

--- ipypublish_plugins/html_ipypublish_chapter.py
+++ ipypublish_plugins/html_ipypublish_chapter.py
@@ -46,13 +46,14 @@ fuzzingbook_tpl_dict = {
 <meta property="og:image" content="__BOOKIMAGE__" />
 
 {# Favicons #}
-<link rel="shortcut icon" href="favicon/favicon.ico" type="image/x-icon">
-<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="152x152" href="favicon/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
 <link rel="manifest" href="favicon/site.webmanifest">
 <link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="shortcut icon" href="favicon/favicon.ico" type="image/x-icon">
 <meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-config" content="favicon/browserconfig.xml">
 <meta name="theme-color" content="#ffffff">
 
 {# Be more mobile-friendly #}
