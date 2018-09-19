df8d09f400a6e2015a194934c1b6acd76ad3e745
Andreas Zeller <zeller@cs.uni-saarland.de>
2018-09-19 08:36:52 +0200

    New: favicons

--- ipypublish_plugins/html_ipypublish_chapter.py
+++ ipypublish_plugins/html_ipypublish_chapter.py
@@ -45,6 +45,16 @@ fuzzingbook_tpl_dict = {
 <meta property="og:description" content="__DESCRIPTION__" />
 <meta property="og:image" content="__BOOKIMAGE__" />
 
+{# Favicons #}
+<link rel="shortcut icon" href="favicon/favicon.ico" type="image/x-icon">
+<link rel="apple-touch-icon" sizes="180x180" href="favicon/apple-touch-icon.png">
+<link rel="icon" type="image/png" sizes="32x32" href="favicon/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="16x16" href="favicon/favicon-16x16.png">
+<link rel="manifest" href="favicon/site.webmanifest">
+<link rel="mask-icon" href="favicon/safari-pinned-tab.svg" color="#5bbad5">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="theme-color" content="#ffffff">
+
 {# Be more mobile-friendly #}
 {# https://nipunbatra.github.io/blog/2017/Jupyter-powered-blog.html #}
 <meta name="viewport" content="width=device-width, initial-scale=1">
