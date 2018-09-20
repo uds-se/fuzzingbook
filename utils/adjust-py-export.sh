1f92c6e97d9fd6256bca17ce516c1249db6ed98c
Andreas Zeller <zeller@cs.uni-saarland.de>
2018-09-20 15:56:29 +0200

    Removed unused scripts

--- utils/adjust-py-export.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Some fixes to exported .py files
-
-sed 's/^import fuzzingbook_utils$/# & # only in notebook/' | \
-sed "s/^get_ipython().run_cell_magic([^,]*, [^,]*, '\([^']*\)').*$/\1/" | \
-sed 's/^get_ipython().*/# & # only in notebook/'
