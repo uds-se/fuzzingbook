b0ade687981e5dc7f85e6a1793cefc52bfad3952
Andreas Zeller <zeller@cs.uni-saarland.de>
2018-10-19 14:28:48 +0200

    Doc update

--- docs/beta/code/GrammarMiner.py
+++ docs/beta/code/GrammarMiner.py
@@ -3,7 +3,7 @@
 
 # This material is part of "Generating Software Tests".
 # Web site: https://www.fuzzingbook.org/html/GrammarMiner.html
-# Last change: 2018-10-19 14:25:17+02:00
+# Last change: 2018-10-19 14:28:26+02:00
 #
 #
 # Copyright (c) 2018 Saarland University, CISPA, authors, and contributors
