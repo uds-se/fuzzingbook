45e2e23203510f6dfcb1e4a4af3a2aa6856d686b
Andreas Zeller <zeller@cs.uni-saarland.de>
2018-10-24 11:58:43 +0200

    Doc update

--- docs/beta/code/GrammarFuzzer.py
+++ docs/beta/code/GrammarFuzzer.py
@@ -3,7 +3,7 @@
 
 # This material is part of "Generating Software Tests".
 # Web site: https://www.fuzzingbook.org/html/GrammarFuzzer.html
-# Last change: 2018-10-20 22:01:13+02:00
+# Last change: 2018-10-24 11:55:41+02:00
 #
 #
 # Copyright (c) 2018 Saarland University, CISPA, authors, and contributors
