3af9a2c452839524490a2713ca7520824ca6c1e8
Andreas Zeller <zeller@cs.uni-saarland.de>
2018-10-29 15:29:45 +0100

    Doc update

--- docs/beta/code/Carver.py
+++ docs/beta/code/Carver.py
@@ -3,7 +3,7 @@
 
 # This material is part of "Generating Software Tests".
 # Web site: https://www.fuzzingbook.org/html/Carver.html
-# Last change: 2018-10-29 14:40:48+01:00
+# Last change: 2018-10-29 14:49:26+01:00
 #
 #
 # Copyright (c) 2018 Saarland University, CISPA, authors, and contributors
