ee881bf79fe680e22cad4bcd87f8a8e87446b278
Andreas Zeller <zeller@cs.uni-saarland.de>
2018-11-02 11:37:03 +0100

    Doc update

--- docs/beta/code/Parser.py
+++ docs/beta/code/Parser.py
@@ -3,7 +3,7 @@
 
 # This material is part of "Generating Software Tests".
 # Web site: https://www.fuzzingbook.org/html/Parser.html
-# Last change: 2018-11-02 11:27:32+01:00
+# Last change: 2018-11-02 11:36:04+01:00
 #
 #
 # Copyright (c) 2018 Saarland University, CISPA, authors, and contributors
@@ -751,13 +751,22 @@ if __name__ == "__main__":
     my_sqrt(2)
 
 
+# ##### Nullable
+
+if __name__ == "__main__":
+    print('\n##### Nullable')
+
+
+
+
+def nullable_expr(expr, nullables): return all(token in nullables for token in expr)
+
 def nullable(grammar):
     productions = rules(grammar)
     @fixpoint
     def nullable_(nullables):
         for A, expr in productions:
-            if all(token in nullables for token in expr):
-                nullables |= {A}
+            if nullable_expr(expr, nullables): nullables |= {A}
         return (nullables)
     return nullable_({EPSILON})
 
@@ -977,20 +986,24 @@ if __name__ == "__main__":
 
 
 
-def firstset(grammar, epsilon):
+def firstset(grammar, nullable):
     first = {i:{i} for i in terminals(grammar)}
     for k in grammar:
-        first[k] = {EPSILON} if k in epsilon else set()
-    return firstset_((rules(grammar), first, epsilon))[1]
+        first[k] = {EPSILON} if k in nullable else set()
+    return firstset_((rules(grammar), first, nullable))[1]
+
+def first_expr(expr, first, nullable):
+    tokens = set()
+    for token in expr:
+        tokens |= first[token]
+        if token not in nullable: break
+    return tokens
 
 @fixpoint
 def firstset_(arg):
     (rules, first, epsilon) = arg
     for A, expression in rules:
-        for token in expression:
-            first[A] |= first[token]
-            if token not in epsilon:
-                break
+        first[A] |= first_expr(expression, first,epsilon)
     return (rules, first, epsilon)
 
 if __name__ == "__main__":
@@ -1030,3 +1043,347 @@ def followset_(arg):
 if __name__ == "__main__":
     followset(canonical(A1_GRAMMAR), START_SYMBOL)
 
+
+# ### Exercise 7 _LL(1) parser_
+
+if __name__ == "__main__":
+    print('\n### Exercise 7 _LL(1) parser_')
+
+
+
+
+def predict(rulepair, first, follow, epsilon):
+    A, rule = rulepair
+    rf = rfirst(rule, first, epsilon)
+    if rnullable(rule, epsilon):
+        rf |= follow[A]
+    return rf
+
+def parse_table(grammar, start, my_rules):
+    _, epsilon, first, follow = followset(grammar, start)
+
+    ptable = [(rule, predict(rule, first, follow, epsilon))
+              for rule in my_rules]
+
+    parse_tbl = {k: {} for k in grammar}
+
+    for (k, expr), pvals in ptable:
+        parse_tbl[k].update({v: (k, expr) for v in pvals})
+    return parse_tbl
+
+def parse_helper(grammar, tbl, stack, inplst):
+    inp, *inplst = inplst
+    exprs = []
+    while stack:
+        val, *stack = stack
+        if isinstance(val, tuple):
+            exprs.append(val)
+        elif val not in grammar:  # terminal
+            assert val == inp
+            exprs.append(val)
+            inp, *inplst = inplst or [None]
+        else:
+            _, rhs = tbl[val][inp] if inp else (None, [])
+            stack = rhs + [(val, len(rhs))] + stack
+    return exprs
+
+def parse(grammar, start, inp):
+    my_rules = rules(grammar)
+    parse_tbl = parse_table(grammar, start, my_rules)
+    k, _ = my_rules[0]
+    stack = [k]
+    return parse_helper(grammar, parse_tbl, stack, list(inp))
+
+def linear_to_tree(arr):
+    stack = []
+    while arr:
+        elt = arr.pop(0)
+        if not isinstance(elt, tuple):
+            stack.append((elt, []))
+        else:
+            # get the last n
+            sym, n = elt
+            elts = stack[-n:] if n > 0 else []
+            stack = stack[0:len(stack) - n]
+            stack.append((sym, elts))
+    assert len(stack) == 1
+    return stack[0]
+
+# ### Exercise 8 _LR(1) parser_
+
+if __name__ == "__main__":
+    print('\n### Exercise 8 _LR(1) parser_')
+
+
+
+
+from enum import Enum, auto
+class Action(Enum):
+    Goto = auto()
+    Shift = auto()
+    Accept = auto()
+    Reduce = auto()
+
+RE_NONTERMINAL = re.compile(r'(<[^<> ]*>)')
+
+class PLine:
+    cache = {}
+    counter = 0
+    fdict = None
+    def __init__(self, key, production, cursor=0, lookahead=set(), pnum=0):
+        self.key,self.production,self.cursor,self.lookahead = key,production,cursor,lookahead
+        self.tokens = self.production
+        self.pnum = pnum
+
+    @classmethod
+    def reset(cls):
+        PLine.cache.clear()
+        PLine.fdict = None
+        PLine.counter = 0
+
+    @classmethod
+    def init_cache(cls, grammar, fdict):
+        PLine.fdict = fdict
+        for key in sorted(grammar.keys()):
+            for production in grammar[key]:
+                PLine.cache[str((key, production, 0))] = PLine(key, production,
+                        cursor=0, lookahead=fdict[key], pnum=PLine.counter)
+                PLine.counter += 1
+        return len(PLine.cache.keys())
+
+    @classmethod
+    def get(cls, key, production, cursor):
+        val = PLine.cache.get(str((key, production, cursor)))
+        if val: return val
+
+        seed = PLine.cache.get(str((key, production, 0)))
+        val = PLine(key, production, cursor, seed.lookahead, seed.pnum)
+        PLine.cache[str((key, production, cursor))] = val
+
+        return val
+
+    @classmethod
+    def from_seed(cls, obj, cursor):
+        return PLine.get(obj.key, obj.production, cursor)
+
+    def production_number(self):
+        return self.pnum
+
+    def __repr__(self): return str(self)
+
+    def __str__(self):
+        return "[p%s]: %s -> %s \tcursor: %s %s" % (self.production_number(),
+                self.key, ''.join([str(i) for i in self.tokens]), self.cursor, '@' + ''.join(sorted(self.lookahead)))
+
+    def advance(self):
+        if self.cursor >= len(self.tokens): return '', None
+        if self.at(self.cursor) == EOF: return '', None
+        token = self.at(self.cursor)
+        return token, PLine.from_seed(self, self.cursor+1)
+
+    def at(self, cursor):
+        if cursor >= len(self.tokens): return None
+        return self.tokens[cursor]
+
+def lr1_closure(closure, cursor, grammar):
+    items = closure[:] # copy
+    seen = set()
+    while items:
+        item, *items = items
+        token = item.at(item.cursor)
+        if not token: continue
+        if token in seen: continue
+        if token in grammar:
+            for ps in grammar[token]:
+                pl = PLine.get(key=token, production=ps, cursor=0)
+                items.append(pl)
+                closure.append(pl)
+                seen.add(pl.key)
+    return closure
+
+class State:
+    counter = 1
+    registry = {}
+    cache = {}
+    def reset():
+        PLine.reset()
+        State.counter = 1
+        State.registry = {}
+        State.cache = {}
+
+    def __init__(self, plines, sfrom=None):
+        self.plines = plines
+        self.shifts = {}
+        self.go_tos = {}
+        self.i = State.counter
+        self.row = []
+        self.hrow = {}
+        self.note = "*"
+        if sfrom:
+            self.grammar = sfrom.grammar
+            self.start = sfrom.start
+        State.counter += 1
+        State.registry[self.i] = self
+        self.key = ''.join([str(l) for l in plines])
+        if State.cache.get(self.key): raise Exception("Cache already has the state. Use State.get")
+        State.cache[self.key] = self
+
+    @classmethod
+    def get(cls, plines, sfrom=None):
+        key = ''.join([str(l) for l in plines])
+        val = State.cache.get(key)
+        if val: return val
+        State.cache[key] = State(plines, sfrom)
+        return State.cache[key]
+
+    def __str__(self):
+        return "State(%s):\n\t%s" % (self.i, "\n\t".join([str(i) for i in self.plines]))
+
+    def __repr__(self): return str(self)
+
+    @classmethod
+    def construct_initial_state(cls, grammar, start=START_SYMBOL):
+        _, _epsilon, _first, follow = followset(grammar, start)
+        PLine.init_cache(grammar, follow)
+        production_str = grammar[start][0]
+
+        pl = PLine.get(key=start, production=production_str, cursor=0)
+
+        lr1_items = lr1_closure(closure=[pl], cursor=0, grammar=grammar)
+        state =  cls(lr1_items, 0)
+        # seed state
+        state.start, state.grammar = start, grammar
+        return state
+
+    def go_to(self, token):
+        if self.go_tos.get(token): return self.go_tos[token]
+        if token not in self.grammar: return None
+        new_plines = []
+        for pline in self.plines:
+            tadv, new_pline = pline.advance()
+            if token == tadv:
+                new_plines.append(new_pline)
+        if not new_plines: return None
+        s = self.form_closure(new_plines)
+        self.go_tos[token] = s
+        s.note = "%s -> [%s] -> " % (self.i,  token)
+        return s
+
+    def shift_to(self, token):
+        if self.shifts.get(token): return self.shifts[token]
+        if token in self.grammar: return None
+        new_plines = []
+        for pline in self.plines:
+            tadv, new_pline = pline.advance()
+            if token == tadv:
+                new_plines.append(new_pline)
+        if not new_plines: return None
+        # each time we shift, we have to build a new closure, with cursor at 0
+        # for the newly added rules.
+        s = self.form_closure(new_plines)
+        self.shifts[token] = s
+        s.note = "%s -> [%s] -> " % (self.i,  token)
+        return s
+
+    def form_closure(self, plines):
+        closure = lr1_closure(closure=plines, cursor=0, grammar=self.grammar)
+        s = State.get(plines=plines, sfrom=self)
+        return s
+
+    def get_reduction(self, nxt_tok):
+        # is the cursor at the end in any of the plines?
+        for pline in self.plines:
+            if pline.cursor + 1 >= len(pline.tokens):
+                res = nxt_tok in pline.lookahead
+                if res: return pline
+        # return the production number too for this pline
+        return None
+
+    @classmethod
+    def construct_states(cls, grammar, start=START_SYMBOL):
+        state1 = State.construct_initial_state(grammar, start)
+        states = [state1]
+        follow = {}
+        all_states = set()
+        seen = set()
+        while states:
+            state, *states = states
+            if state.i in seen: continue
+            seen.add(state.i)
+            all_states.add(state)
+            sym = symbols(grammar)
+            for key in sorted(sym): # needs terminal symbols too.
+                if key not in grammar:
+                    new_state = state.shift_to(key)
+                    if new_state: # and new_state.i not in seen:
+                        states.append(new_state)
+                        state.hrow[key] = (Action.Shift, new_state.i)
+                    else:
+                        state.hrow[key] = ('_', None)
+                else:
+                    new_state = state.go_to(key)
+                    if new_state: # and new_state.i not in seen:
+                        states.append(new_state)
+                        state.hrow[key] = (Action.Goto, new_state.i)
+                    else:
+                        state.hrow[key] = ('_', None)
+
+        for state in all_states:
+            for line in state.plines:
+                if line.at(line.cursor) == EOF:
+                    key = EOF 
+                    state.hrow[key] = (Action.Accept, None)
+                elif line.cursor + 1 > len(line.tokens):
+                    for key in line.lookahead:
+                        state.hrow[key] = (Action.Reduce, line)
+        return state1
+
+def parse(input_text, grammar):
+    expr_stack = []
+    state_stack = [State.registry[1].i]
+    tokens = list(input_text)
+    next_token = None
+    tree = []
+    while True:
+        if not next_token:
+            if not tokens:
+                next_token = EOF
+            else:
+                next_token, *tokens = tokens
+        # use the next_token on the state stack to decide what to do.
+        (action, nxt) = State.registry[state_stack[-1]].hrow[next_token]
+        if action == Action.Shift:
+            next_state = State.registry[nxt]
+            # this means we can shift.
+            expr_stack.append(next_token)
+            state_stack.append(next_state.i)
+            next_token = None
+        elif action == Action.Reduce:
+            pline = nxt
+            # Remove the matched topmost L symbols (and parse trees and
+            # associated state numbers) from the parse stack.
+            # pop the plines' rhs symbols off the stack
+            pnum = len(pline.tokens)
+            popped = expr_stack[-pnum:]
+            expr_stack = expr_stack[:-pnum]
+            # push the lhs symbol of pline
+            expr_stack.append({pline.key: popped})
+            # pop the same number of states.
+            state_stack = state_stack[:-pnum]
+            (action, nxt) = State.registry[state_stack[-1]].hrow[pline.key]
+            next_state = State.registry[nxt]
+            state_stack.append(next_state.i)
+        elif action == Action.Goto:
+            next_state = State.registry[nxt]
+            state_stack.append(next_state.i)
+        elif action == Action.Accept:
+            break
+        else:
+            raise Exception("Syntax error")
+
+    assert len(expr_stack) == 1
+    return expr_stack[0]
+
+def initialize(grammar, start):
+    grammar[start][0].append(EOF)
+    State.construct_states(grammar, start)
