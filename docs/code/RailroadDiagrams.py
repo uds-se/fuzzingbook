402116fc19f89d1d572afc99a048ac97ed8f4013
Andreas Zeller <zeller@cs.uni-saarland.de>
2018-11-29 11:52:06 +0100

    Doc update

--- docs/code/RailroadDiagrams.py
+++ docs/code/RailroadDiagrams.py
@@ -3,7 +3,7 @@
 
 # This material is part of "Generating Software Tests".
 # Web site: https://www.fuzzingbook.org/html/RailroadDiagrams.html
-# Last change: 2018-11-27 06:43:18-08:00
+# Last change: 2018-11-27 15:25:09+01:00
 #
 #
 # Copyright (c) 2018 Saarland University, CISPA, authors, and contributors
