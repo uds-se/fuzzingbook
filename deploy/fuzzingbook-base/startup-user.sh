c02e0d577403832be2d4025d84af5c7d4a2bdab2
Christian Holler (:decoder) <choller@mozilla.com>
2018-12-14 20:24:28 +0100

    Run git pull on startup

--- deploy/fuzzingbook-base/startup-user.sh
+++ deploy/fuzzingbook-base/startup-user.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
+
+# TODO: Eventually we should remove this and pin fuzzingbook to a specific version at build time
+cd /home/${FB_USER}/fuzzingbook
+git config --global user.email "fuzzingbook@example.com"
+git config --global user.name "Fuzzingbook Docker"
+git stash
+git pull origin master
+git stash apply
+# if conflict on pop:
+git merge --strategy-option ours
+
 cd /home/${FB_USER}/FuzzManager/server/
 celery -A celeryconf -l INFO -n worker@%h -Q celery worker &
 python3 manage.py runserver 0.0.0.0:8000 &
