7d636865d2a2f19213aaf4ff4c160c4f616259d8
Andreas Zeller <zeller@cs.uni-saarland.de>
2019-01-22 14:15:23 +0100

    Doc update

--- docs/beta/code/Carver.py
+++ docs/beta/code/Carver.py
@@ -3,7 +3,7 @@
 
 # This material is part of "Generating Software Tests".
 # Web site: https://www.fuzzingbook.org/html/Carver.html
-# Last change: 2018-11-18 14:40:05+01:00
+# Last change: 2019-01-17 10:09:01+01:00
 #
 #
 # Copyright (c) 2018 Saarland University, CISPA, authors, and contributors
@@ -44,17 +44,20 @@ if __name__ == "__main__":
 
 
 
-import urllib.parse
-import urllib.request
+if __name__ == "__main__":
+    # We use the same fixed seed as the notebook to ensure consistency
+    import random
+    random.seed(2001)
 
-import fuzzingbook_utils
+
+import urllib.parse
+
+import requests
 
 def webbrowser(url):
     """Download the http/https resource given by the URL"""
-    response = urllib.request.urlopen(url)
-    if response.getcode() == 200:
-        contents = response.read()
-    return contents.decode("utf8")
+    r = requests.get(url)
+    return r.text
 
 if __package__ is None or __package__ == "":
     from Timer import Timer
@@ -260,7 +263,12 @@ if __name__ == "__main__":
 
 
 if __name__ == "__main__":
-    print(webbrowser_carver.called_functions(qualified=True))
+    function_list = webbrowser_carver.called_functions(qualified=True)
+    len(function_list)
+
+
+if __name__ == "__main__":
+    print(function_list[:50])
 
 
 if __name__ == "__main__":
@@ -400,6 +408,331 @@ if __name__ == "__main__":
 if __name__ == "__main__":
     for i in range(10):
         print(list(exceptions_seen)[i])
+
+
+# ## Mining API Grammars from Carved Calls
+
+if __name__ == "__main__":
+    print('\n## Mining API Grammars from Carved Calls')
+
+
+
+
+# ### From Calls to Grammars
+
+if __name__ == "__main__":
+    print('\n### From Calls to Grammars')
+
+
+
+
+import math
+
+def power(x, y):
+    return math.pow(x, y)
+
+if __name__ == "__main__":
+    with CallCarver() as power_carver:
+        z = power(1, 2)
+        z = power(3, 4)
+
+
+if __name__ == "__main__":
+    power_carver.arguments("power")
+
+
+if __package__ is None or __package__ == "":
+    from Grammars import START_SYMBOL, is_valid_grammar, new_symbol, extend_grammar
+else:
+    from .Grammars import START_SYMBOL, is_valid_grammar, new_symbol, extend_grammar
+
+
+POWER_GRAMMAR = {
+    "<start>": ["power(<x>, <y>)"],
+    "<x>": ["1", "3"],
+    "<y>": ["2", "4"]
+}
+
+assert is_valid_grammar(POWER_GRAMMAR)
+
+if __package__ is None or __package__ == "":
+    from GrammarCoverageFuzzer import GrammarCoverageFuzzer
+else:
+    from .GrammarCoverageFuzzer import GrammarCoverageFuzzer
+
+
+if __name__ == "__main__":
+    power_fuzzer = GrammarCoverageFuzzer(POWER_GRAMMAR)
+    [power_fuzzer.fuzz() for i in range(5)]
+
+
+# ### A Grammar Miner for Calls
+
+if __name__ == "__main__":
+    print('\n### A Grammar Miner for Calls')
+
+
+
+
+class CallGrammarMiner(object):
+    def __init__(self, carver, log=False):
+        self.carver = carver
+        self.log = log
+
+# #### Initial Grammar
+
+if __name__ == "__main__":
+    print('\n#### Initial Grammar')
+
+
+
+
+import copy 
+
+class CallGrammarMiner(CallGrammarMiner):
+    CALL_SYMBOL = "<call>"
+
+    def initial_grammar(self):
+        return extend_grammar(
+            {START_SYMBOL: [self.CALL_SYMBOL],
+                self.CALL_SYMBOL: []
+             })
+
+if __name__ == "__main__":
+    m = CallGrammarMiner(power_carver)
+    initial_grammar = m.initial_grammar()
+    initial_grammar
+
+
+# #### A Grammar from Arguments
+
+if __name__ == "__main__":
+    print('\n#### A Grammar from Arguments')
+
+
+
+
+if __name__ == "__main__":
+    arguments = power_carver.arguments("power")
+    arguments
+
+
+class CallGrammarMiner(CallGrammarMiner):
+    def var_symbol(self, function_name, var, grammar):
+        return new_symbol(grammar, "<" + function_name + "-" + var + ">")
+
+    def mine_arguments_grammar(self, function_name, arguments, grammar):
+        var_grammar = {}
+
+        variables = {}
+        for argument_list in arguments:
+            for (var, value) in argument_list:
+                value_string = call_value(value)
+                if self.log:
+                    print(var, "=", value_string)
+
+                if value_string.find("<") >= 0:
+                    var_grammar["<langle>"] = ["<"]
+                    value_string = value_string.replace("<", "<langle>")
+
+                if var not in variables:
+                    variables[var] = set()
+                variables[var].add(value_string)
+
+        var_symbols = []
+        for var in variables:
+            var_symbol = self.var_symbol(function_name, var, grammar)
+            var_symbols.append(var_symbol)
+            var_grammar[var_symbol] = list(variables[var])
+
+        return var_grammar, var_symbols
+
+if __name__ == "__main__":
+    m = CallGrammarMiner(power_carver)
+    var_grammar, var_symbols = m.mine_arguments_grammar(
+        "power", arguments, initial_grammar)
+
+
+if __name__ == "__main__":
+    var_grammar
+
+
+if __name__ == "__main__":
+    var_symbols
+
+
+# #### A Grammar from Calls
+
+if __name__ == "__main__":
+    print('\n#### A Grammar from Calls')
+
+
+
+
+class CallGrammarMiner(CallGrammarMiner):
+    def function_symbol(self, function_name, grammar):
+        return new_symbol(grammar, "<" + function_name + ">")
+
+    def mine_function_grammar(self, function_name, grammar):
+        arguments = self.carver.arguments(function_name)
+
+        if self.log:
+            print(function_name, arguments)
+
+        var_grammar, var_symbols = self.mine_arguments_grammar(
+            function_name, arguments, grammar)
+
+        function_grammar = var_grammar
+        function_symbol = self.function_symbol(function_name, grammar)
+
+        if len(var_symbols) > 0 and var_symbols[0].find("-self") >= 0:
+            # Method call
+            function_grammar[function_symbol] = [
+                var_symbols[0] + "." + function_name + "(" + ", ".join(var_symbols[1:]) + ")"]
+        else:
+            function_grammar[function_symbol] = [
+                function_name + "(" + ", ".join(var_symbols) + ")"]
+
+        if self.log:
+            print(function_symbol, "::=", function_grammar[function_symbol])
+
+        return function_grammar, function_symbol
+
+if __name__ == "__main__":
+    m = CallGrammarMiner(power_carver)
+    function_grammar, function_symbol = m.mine_function_grammar(
+        "power", initial_grammar)
+    function_grammar
+
+
+if __name__ == "__main__":
+    function_symbol
+
+
+# #### A Grammar from all Calls
+
+if __name__ == "__main__":
+    print('\n#### A Grammar from all Calls')
+
+
+
+
+if __name__ == "__main__":
+    power_carver.called_functions()
+
+
+class CallGrammarMiner(CallGrammarMiner):
+    def mine_call_grammar(self, function_list=None, qualified=False):
+        grammar = self.initial_grammar()
+        fn_list = function_list
+        if function_list is None:
+            fn_list = self.carver.called_functions(qualified=qualified)
+
+        for function_name in fn_list:
+            if function_list is None and (function_name.startswith("_") or function_name.startswith("<")):
+                continue  # Internal function
+
+            # Ignore errors with mined functions
+            try:
+                function_grammar, function_symbol = self.mine_function_grammar(
+                    function_name, grammar)
+            except:
+                if function_list is not None:
+                    raise
+
+            if function_symbol not in grammar[self.CALL_SYMBOL]:
+                grammar[self.CALL_SYMBOL].append(function_symbol)
+            grammar.update(function_grammar)
+
+        assert is_valid_grammar(grammar)
+        return grammar
+
+if __name__ == "__main__":
+    m = CallGrammarMiner(power_carver)
+    power_grammar = m.mine_call_grammar()
+    power_grammar
+
+
+if __name__ == "__main__":
+    power_fuzzer = GrammarCoverageFuzzer(power_grammar)
+    [power_fuzzer.fuzz() for i in range(5)]
+
+
+# ## Fuzzing Web Functions
+
+if __name__ == "__main__":
+    print('\n## Fuzzing Web Functions')
+
+
+
+
+if __package__ is None or __package__ == "":
+    from Carver import webbrowser
+else:
+    from .Carver import webbrowser
+
+
+if __name__ == "__main__":
+    with CallCarver() as webbrowser_carver:
+        webbrowser("https://www.fuzzingbook.org")
+        webbrowser("http://www.example.com")
+
+
+if __name__ == "__main__":
+    m = CallGrammarMiner(webbrowser_carver)
+    webbrowser_grammar = m.mine_call_grammar()
+
+
+if __name__ == "__main__":
+    call_list = webbrowser_grammar['<call>']
+    len(call_list)
+
+
+if __name__ == "__main__":
+    print(call_list[:20])
+
+
+if __name__ == "__main__":
+    webbrowser_grammar["<urlsplit>"]
+
+
+if __name__ == "__main__":
+    webbrowser_grammar["<urlsplit-url>"]
+
+
+if __name__ == "__main__":
+    webbrowser_grammar["<urlsplit-scheme>"]
+
+
+if __name__ == "__main__":
+    urlsplit_fuzzer = GrammarCoverageFuzzer(
+        webbrowser_grammar, start_symbol="<urlsplit>")
+    for i in range(5):
+        print(urlsplit_fuzzer.fuzz())
+
+
+from urllib.parse import urlsplit
+
+if __package__ is None or __package__ == "":
+    from Timer import Timer
+else:
+    from .Timer import Timer
+
+
+if __name__ == "__main__":
+    with Timer() as urlsplit_timer:
+        urlsplit('http://www.fuzzingbook.org/', 'http', True)
+    urlsplit_timer.elapsed_time()
+
+
+if __name__ == "__main__":
+    with Timer() as webbrowser_timer:
+        webbrowser("http://www.fuzzingbook.org")
+    webbrowser_timer.elapsed_time()
+
+
+if __name__ == "__main__":
+    webbrowser_timer.elapsed_time() / urlsplit_timer.elapsed_time()
 
 
 # ## Lessons Learned
@@ -562,4 +895,12 @@ if __name__ == "__main__":
             path='',
             query='',
         fragment='')
+
+
+# ### Exercise 2: Abstracting Arguments
+
+if __name__ == "__main__":
+    print('\n### Exercise 2: Abstracting Arguments')
+
+
 
