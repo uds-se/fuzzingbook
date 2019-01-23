f5b88e3e3e8eac50b9a448b41031d53d010e7dd6
Rahul Gopinath <rahul@gopinath.org>
2019-01-23 11:09:20 -0800

    Doc update

--- docs/code/APIFuzzer.py
+++ docs/code/APIFuzzer.py
@@ -3,7 +3,7 @@
 
 # This material is part of "Generating Software Tests".
 # Web site: https://www.fuzzingbook.org/html/APIFuzzer.html
-# Last change: 2019-01-15 18:40:00+01:00
+# Last change: 2019-01-15 10:00:41-08:00
 #
 #
 # Copyright (c) 2018 Saarland University, CISPA, authors, and contributors
