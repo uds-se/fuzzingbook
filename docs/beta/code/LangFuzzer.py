0bd9480bcee425f175425de733d7e1ddd10afc83
Andreas Zeller <zeller@cispa.saarland>
2019-02-04 17:35:19 +0100

    Doc update

--- docs/beta/code/LangFuzzer.py
+++ docs/beta/code/LangFuzzer.py
@@ -3,7 +3,7 @@
 
 # This material is part of "Generating Software Tests".
 # Web site: https://www.fuzzingbook.org/html/LangFuzzer.html
-# Last change: 2019-02-04 15:45:22+01:00
+# Last change: 2019-02-04 17:30:15+01:00
 #
 #
 # Copyright (c) 2018 Saarland University, CISPA, authors, and contributors
