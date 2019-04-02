0b1aa725fcae5f6871af5fbf3238f19581ca2bc1
Andreas Zeller <zeller@cispa.saarland>
2019-04-02 22:50:55 +1300

    Doc update

--- docs/code/Grammars.py
+++ docs/code/Grammars.py
@@ -3,7 +3,7 @@
 
 # This material is part of "Generating Software Tests".
 # Web site: https://www.fuzzingbook.org/html/Grammars.html
-# Last change: 2019-03-12 05:16:31-07:00
+# Last change: 2019-04-02 22:40:00+13:00
 #
 #
 # Copyright (c) 2018 Saarland University, CISPA, authors, and contributors
