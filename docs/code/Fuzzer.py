04a452771d6896d8c4276989b99c8732ea613032
Andreas Zeller <zeller@cispa.saarland>
2019-04-10 09:21:28 +0800

    Doc update

--- docs/code/Fuzzer.py
+++ docs/code/Fuzzer.py
@@ -3,7 +3,7 @@
 
 # This material is part of "Generating Software Tests".
 # Web site: https://www.fuzzingbook.org/html/Fuzzer.html
-# Last change: 2019-04-02 11:34:02+13:00
+# Last change: 2019-04-10 07:04:51+08:00
 #
 #
 # Copyright (c) 2018 Saarland University, CISPA, authors, and contributors
