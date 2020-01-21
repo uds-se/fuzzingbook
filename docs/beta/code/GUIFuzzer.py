455e79603e2c327c12454debcef33ec2d2f7204f
Andreas Zeller <zeller@cispa.saarland>
2020-01-21 11:56:07 +0100

    Doc update

--- docs/beta/code/GUIFuzzer.py
+++ docs/beta/code/GUIFuzzer.py
@@ -3,7 +3,7 @@
 
 # This material is part of "The Fuzzing Book".
 # Web site: https://www.fuzzingbook.org/html/GUIFuzzer.html
-# Last change: 2019-11-20 17:33:42+01:00
+# Last change: 2020-01-21 11:03:38+01:00
 #
 #!/
 # Copyright (c) 2018-2019 Saarland University, CISPA, authors, and contributors
