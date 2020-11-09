ddf1e6c919cbdac218b100504ec12e3ad5022280
kuznetsov <kuznetsov@st.cs.uni-saarland.de>
2020-11-09 16:41:23 +0100

    vars renaming in bookutils __init__::print_content()

--- notebooks/bookutils/__init__.py
+++ notebooks/bookutils/__init__.py
@@ -122,17 +122,17 @@ def print_content(content, filename=None, lexer=None, start_line_number=None):
                 lexer = get_lexer_for_filename(filename)
 
         colorful_content = highlight(content, lexer, formatters.TerminalFormatter())
-        content = colorful_content
+        content = colorful_content.strip()
     if start_line_number is None:
         print(content, end="")
     else:
-        colorful_content_list = content.strip().split("\n")
-        no_of_lines = len(colorful_content_list)
+        content_list = content.split("\n")
+        no_of_lines = len(content_list)
         size_of_lines_nums = len(str(start_line_number + no_of_lines))
-        for i, line in enumerate(colorful_content_list):
-            colorful_content_list[i] = ('{0:' + str(size_of_lines_nums) + '}:').format(i + start_line_number) + " " + line
-        colorful_content_with_line_no = '\n'.join(colorful_content_list)
-        print(colorful_content_with_line_no, end="")
+        for i, line in enumerate(content_list):
+            content_list[i] = ('{0:' + str(size_of_lines_nums) + '}:').format(i + start_line_number) + " " + line
+        content_with_line_no = '\n'.join(content_list)
+        print(content_with_line_no, end="")
 
 
 # Escaping unicode characters into ASCII for user-facing strings
