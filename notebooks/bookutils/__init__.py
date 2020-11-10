8275d62dd48088270c95f0031e67df0d7bf9a661
Andreas Zeller <zeller@cispa.saarland>
2020-11-10 10:52:39 +0100

    New: getsourcelines()

--- notebooks/bookutils/__init__.py
+++ notebooks/bookutils/__init__.py
@@ -137,6 +137,26 @@ def print_content(content, filename=None, lexer=None, start_line_number=None):
         content_with_line_no = '\n'.join(content_list)
         print(content_with_line_no, end="")
 
+def getsourcelines(function):
+    """A replacement for inspect.getsourcelines(), but with syntax highlighting"""
+    import inspect
+    
+    source_lines, starting_line_number = \
+       inspect.getsourcelines(function)
+       
+    if not rich_output():
+        return source_lines, starting_line_number
+        
+    from pygments import highlight, lexers, formatters
+    from pygments.lexers import get_lexer_for_filename
+    
+    lexer = get_lexer_for_filename('.py')
+    colorful_content = highlight(
+        "".join(source_lines), lexer,
+        formatters.TerminalFormatter())
+    content = colorful_content.strip()
+    return [line + '\n' for line in content.split('\n')], starting_line_number
+
 
 # Escaping unicode characters into ASCII for user-facing strings
 def unicode_escape(s, error="backslashreplace"):
