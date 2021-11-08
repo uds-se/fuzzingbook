cb327116509fc4bf1274b23cf62edcdb982ef560
Andreas Zeller <zeller@cispa.de>
2021-11-08 08:12:40 +0100

    Update from debuggingbook

--- notebooks/shared/utils/nbdepend.py
+++ notebooks/shared/utils/nbdepend.py
@@ -170,19 +170,32 @@ def draw_notebook_dependencies(notebooks,
         dot.subgraph(cluster)
     
     if transitive_reduction:
+        # Reduce the graph using 'tred'
         dot.format = 'gv'
         dot.save('depend.gv')
         os.system('tred depend.gv > depend.gv~ && mv depend.gv~ depend.gv')
         dot = Source.from_file('depend.gv')
-        os.remove('depend.gv')
 
+        # Clean up
+        try:
+            os.remove('depend.gv~')
+        except FileNotFoundError:
+            pass
+        try:
+            os.remove('depend.gv')
+        except FileNotFoundError:
+            pass
+
+    # Render the graph
     dot.format = format
     dot.render('depend')
     
+    # Print on standard output
     with open('depend.' + format, 'r') as file:
         for line in file:
              print(line, end="")
 
+    # Clean up
     try:
         os.remove('depend')
     except FileNotFoundError:
