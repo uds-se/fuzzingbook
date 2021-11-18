33ea31232ed8b4c15ab2c02e3a78bb1f50ec894b
Andreas Zeller <zeller@cispa.de>
2021-11-18 11:33:23 +0100

    Doc update

--- docs/code/APIFuzzer.py
+++ docs/code/APIFuzzer.py
@@ -3,7 +3,7 @@
 
 # "Fuzzing APIs" - a chapter of "The Fuzzing Book"
 # Web site: https://www.fuzzingbook.org/html/APIFuzzer.html
-# Last change: 2021-11-16 14:38:10+01:00
+# Last change: 2021-11-18 11:29:28+01:00
 #
 # Copyright (c) 2021 CISPA Helmholtz Center for Information Security
 # Copyright (c) 2018-2020 Saarland University, authors, and contributors
