0d425e18f564a8f8d76f4e7e067dcaa906bfa974
Andreas Zeller <zeller@cispa.de>
2021-12-14 18:17:39 +0100

    Doc update

--- docs/beta/code/PrototypingWithPython.py
+++ docs/beta/code/PrototypingWithPython.py
@@ -3,7 +3,7 @@
 
 # "Prototyping with Python" - a chapter of "The Fuzzing Book"
 # Web site: https://www.fuzzingbook.org/html/PrototypingWithPython.html
-# Last change: 2021-12-13 17:02:14+01:00
+# Last change: 2021-12-14 18:02:23+01:00
 #
 # Copyright (c) 2021 CISPA Helmholtz Center for Information Security
 # Copyright (c) 2018-2020 Saarland University, authors, and contributors
