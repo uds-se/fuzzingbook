e7f8532cecafea3c844bc4b2b4ec6d82fbfe4369
Andreas Zeller <zeller@cispa.de>
2022-02-01 17:31:19 +0100

    Doc update

--- docs/code/Reducer.py
+++ docs/code/Reducer.py
@@ -3,7 +3,7 @@
 
 # "Reducing Failure-Inducing Inputs" - a chapter of "The Fuzzing Book"
 # Web site: https://www.fuzzingbook.org/html/Reducer.html
-# Last change: 2022-01-12 14:49:57+01:00
+# Last change: 2022-02-01 17:27:05+01:00
 #
 # Copyright (c) 2021 CISPA Helmholtz Center for Information Security
 # Copyright (c) 2018-2020 Saarland University, authors, and contributors
@@ -98,6 +98,10 @@ if __name__ == '__main__':
     print('# Reducing Failure-Inducing Inputs')
 
 
+
+if __name__ == '__main__':
+    from .bookutils import YouTubeVideo
+    YouTubeVideo('noJUPjSJVh0')
 
 ## Synopsis
 ## --------
