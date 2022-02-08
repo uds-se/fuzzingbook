09e8b5d85bb6789a92aebaf0977a5ac603b8842e
Andreas Zeller <zeller@cispa.de>
2022-02-08 18:58:39 +0100

    Doc update

--- docs/beta/code/Tours.py
+++ docs/beta/code/Tours.py
@@ -3,7 +3,7 @@
 
 # "Tours through the Book" - a chapter of "The Fuzzing Book"
 # Web site: https://www.fuzzingbook.org/html/Tours.html
-# Last change: 2022-02-03 14:30:13+01:00
+# Last change: 2022-02-08 18:50:02+01:00
 #
 # Copyright (c) 2021 CISPA Helmholtz Center for Information Security
 # Copyright (c) 2018-2020 Saarland University, authors, and contributors
