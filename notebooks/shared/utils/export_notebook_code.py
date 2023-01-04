33a40ce3c50bd7939eb30b753d5ff30edb901760
Andreas Zeller <zeller@cispa.de>
2023-01-04 12:29:27 +0100

    Update from debuggingbook

--- notebooks/shared/utils/export_notebook_code.py
+++ notebooks/shared/utils/export_notebook_code.py
@@ -50,7 +50,7 @@ HEADER = """#!/usr/bin/env python3
 # Web site: https://www.{project}.org/html/{module}.html
 # Last change: {timestamp}
 #
-# Copyright (c) 2021 CISPA Helmholtz Center for Information Security
+# Copyright (c) 2021-2023 CISPA Helmholtz Center for Information Security
 # Copyright (c) 2018-2020 Saarland University, authors, and contributors
 #
 # Permission is hereby granted, free of charge, to any person obtaining a
