a726cd1f594d4f11af817444bdea9be3979a0c92
Andreas Zeller <zeller@cispa.de>
2025-01-22 09:34:04 +0100

    Update from debuggingbook

--- notebooks/shared/bookutils/setup.py
+++ notebooks/shared/bookutils/setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
+# Special setup code for fuzzingbook and debuggingbook notebooks
 
-# We set a fixed seed for all notebooks and examples,
-# such that creation is more predictable.
-
+# We set a fixed seed for all notebooks and examples, such that creation
+# is more predictable.
 # Interacting with a notebook (in particular executing cells again and again)
 # will still yield new results every time.
 
@@ -10,3 +10,11 @@ FIXED_SEED = 2001
 
 import random
 random.seed(FIXED_SEED)
+
+
+# We disable automatic garbage collection for notebooks.
+# As of Python 3.12, automatic gc in iPython may invoke a method
+# _clean_thread_parent_frames(), interfering with tracing.
+
+import gc
+gc.disable()
