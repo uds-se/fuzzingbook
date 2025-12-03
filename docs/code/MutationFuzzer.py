a4c5fd36db5727207327097712e4e3b6c168284c
alex9849 <alexander@liggesmeyer.net>
2025-12-03 15:41:30 +0100

    Fixed a bug in FunctionCoverageRunner

--- docs/code/MutationFuzzer.py
+++ docs/code/MutationFuzzer.py
@@ -423,12 +423,12 @@ from .Coverage import Coverage, population_coverage, Location
 
 class FunctionCoverageRunner(FunctionRunner):
     def run_function(self, inp: str) -> Any:
-        with Coverage() as cov:
-            try:
+        try:
+            with Coverage() as cov:
                 result = super().run_function(inp)
-            except Exception as exc:
-                self._coverage = cov.coverage()
-                raise exc
+        except Exception as exc:
+            self._coverage = cov.coverage()
+            raise exc
 
         self._coverage = cov.coverage()
         return result
